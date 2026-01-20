1595be860df9dd18f3b92094bbae4a1be0a16e31
Benarjeechowdary <115145711+Benarjeechowdary@users.noreply.github.com>
2026-01-20 16:16:53 +0530

    modified files

--- __temp_pipreqsnb_folder/_pipreqsnb_temp_file.py
+++ __temp_pipreqsnb_folder/_pipreqsnb_temp_file.py
@@ -21,3 +21,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 import joblib
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+import joblib
